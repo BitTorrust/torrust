141d0f98d36984a723c82f6c250f0b8496e02f3d
Jean Fontena <diegofontena@gmail.com>
2022-10-18 08:46:23 +0200

    Use paths relative to the project location

--- scripts/seed-icerberg.sh
+++ scripts/seed-icerberg.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-ICEBERG_FOLDER="$HOME/projects/projet-reseau/torrent-iceberg"
-OPENTRACKER_FOLDER="$HOME/projects/projet-reseau/opentracker"
+ICEBERG_FOLDER="./samples"
 OPENTRACKER_IP_ADDRESS="127.0.0.1"
 OPENTRACKER_PORT=6969
 
-$OPENTRACKER_FOLDER/opentracker -i $OPENTRACKER_IP_ADDRESS -p $OPENTRACKER_PORT & \
+opentracker -i $OPENTRACKER_IP_ADDRESS -p $OPENTRACKER_PORT & \
 aria2c -V -d $ICEBERG_FOLDER $ICEBERG_FOLDER/iceberg.jpg.torrent

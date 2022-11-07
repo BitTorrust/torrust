05f47f9546711804f0a9a20f0f4bdc8b35909756
Julien Liottard <julien.liottard@grenoble-inp.org>
2022-11-07 09:14:34 +0100

    fix main including cli parsing

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,30 @@ pub use torrent::Torrent;
 mod error;
 use error::Error;
 
+mod cli;
+
+use clap::Parser;
+use cli::Args;
+use std::fs;
+
 #[cfg(test)]
 mod tests;
 
 fn main() -> Result<(), Error> {
+    let args = Args::parse();
+    let torrent_file_content = fs::read(args.torrent_file()).unwrap();
+
+    if args.info() {
+        println!("INFO flag set");
+    }
+
+    if args.debug() {
+        println!("DEBUG flag set");
+    }
+
+    // Main program
+    println!("\nWorking directory: {:?}\n", args.working_directory());
+    println!("Torrent file content: {:?}", torrent_file_content);
+
     Ok(())
 }

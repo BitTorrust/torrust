fea90d722ad68d947603936d9adcdda83be4050b
Jean Fontena <diegofontena@gmail.com>
2022-11-07 12:02:51 +0100

    Create Event file

--- src/http.rs
+++ src/http.rs
@@ -1,5 +1,7 @@
+mod event;
 mod tracker_request;
 mod tracker_response;
 
-pub use tracker_request::{Event, TrackerRequest};
+pub use event::Event;
+pub use tracker_request::TrackerRequest;
 pub use tracker_response::{Peer, TrackerResponse};
--- src/http/tracker_request.rs
+++ src/http/tracker_request.rs
@@ -1,4 +1,4 @@
-use {crate::Error, reqwest::Url, std::fmt};
+use {crate::http::Event, crate::Error, reqwest::Url};
 
 pub struct TrackerRequest {
     info_hash: [u8; 20],
@@ -61,23 +61,5 @@ impl TrackerRequest {
             url.query_pairs_mut()
                 .append_pair("event", &event.to_string());
         }
-    }
-}
-
-pub enum Event {
-    Started,
-    Stopped,
-    Completed,
-}
-
-impl fmt::Display for Event {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let text = match self {
-            Event::Started => "started",
-            Event::Stopped => "stopped",
-            Event::Completed => "completed",
-        };
-
-        write!(f, "{}", text)
     }
 }

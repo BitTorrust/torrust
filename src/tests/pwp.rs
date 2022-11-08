bba8d44284cf9bce8f3cf7f7ad0a86f603147e54
Jean Fontena <diegofontena@gmail.com>
2022-11-08 16:31:49 +0100

    Refactor tests using a path_builder method to avoid repeating the path

--- src/tests/pwp.rs
+++ src/tests/pwp.rs
@@ -24,11 +24,13 @@ pub mod unittest {
         data_buffer
     }
 
+    fn path_builder(file: &str) -> String {
+        "samples/peer_wire_protocol-messages/".to_string() + file
+    }
+
     #[test]
     pub fn handshake_message_into_bytes() {
-        let expected = read_bytes_from(
-            "samples/peer_wire_protocol-messages/expected_handshake_bytes_in_hex.bin",
-        );
+        let expected = read_bytes_from(&path_builder("expected_handshake_bytes_in_hex.bin"));
         let handshake_bytes = Handshake::new(INFO_ID, PEER_ID);
 
         assert_eq!(handshake_bytes.into_bytes(), expected);
@@ -36,9 +38,7 @@ pub mod unittest {
 
     #[test]
     pub fn unchoke_message_into_bytes() {
-        let expected = read_bytes_from(
-            "samples/peer_wire_protocol-messages/expected_unchoke_bytes_in_hex.bin",
-        );
+        let expected = read_bytes_from(&path_builder("expected_unchoke_bytes_in_hex.bin"));
         let unchoke_message = Unchoke::new();
 
         assert_eq!(unchoke_message.into_bytes(), expected);
@@ -46,9 +46,7 @@ pub mod unittest {
 
     #[test]
     pub fn interested_message_into_bytes() {
-        let expected = read_bytes_from(
-            "samples/peer_wire_protocol-messages/expected_interested_bytes_in_hex.bin",
-        );
+        let expected = read_bytes_from(&path_builder("expected_interested_bytes_in_hex.bin"));
         let interested_message = Interested::new();
 
         assert_eq!(interested_message.into_bytes(), expected);
@@ -56,25 +54,17 @@ pub mod unittest {
 
     #[test]
     pub fn bitfield_message_into_bytes() {
-        let expected = read_bytes_from(
-            "samples/peer_wire_protocol-messages/expected_bitfield_bytes_in_hex.bin",
-        );
-
-        let bitfield = BitVec::from_bytes(&[0xff, 0xe0]);
-        let bitfield_message = Bitfield::new(bitfield);
+        let expected = read_bytes_from(&path_builder("expected_bitfield_bytes_in_hex.bin"));
+        let bitfield_message = Bitfield::new(BitVec::from_bytes(&[0xff, 0xe0]));
 
         assert_eq!(bitfield_message.into_bytes(), expected);
     }
 
     #[test]
     pub fn piece_message_into_bytes() {
-        // Init Piece struct
-        let data =
-            read_bytes_from("samples/peer_wire_protocol-messages/data_pieces_bytes_in_hex.bin");
+        let data = read_bytes_from(&path_builder("data_pieces_bytes_in_hex.bin"));
         let piece_message = Piece::new(6, 0, data);
-
-        let expected_bytes =
-            read_bytes_from("samples/peer_wire_protocol-messages/expected_pieces_bytes_in_hex.bin");
+        let expected_bytes = read_bytes_from(&path_builder("expected_pieces_bytes_in_hex.bin"));
 
         assert_eq!(piece_message.into_bytes(), expected_bytes);
     }
@@ -82,10 +72,7 @@ pub mod unittest {
     #[test]
     pub fn request_message_into_bytes() {
         let request_message = Request::new(6, 0, 0x4000);
-
-        let expected_bytes = read_bytes_from(
-            "samples/peer_wire_protocol-messages/expected_request_bytes_in_hex.bin",
-        );
+        let expected_bytes = read_bytes_from(&path_builder("expected_request_bytes_in_hex.bin"));
 
         assert_eq!(request_message.into_bytes(), expected_bytes);
     }
@@ -93,10 +80,8 @@ pub mod unittest {
     #[test]
     pub fn not_interested_message_into_bytes() {
         let not_interested_message = NotIterested::new();
-
-        let expected_bytes = read_bytes_from(
-            "samples/peer_wire_protocol-messages/expected_not_interested_bytes_in_hex.bin",
-        );
+        let expected_bytes =
+            read_bytes_from(&path_builder("expected_not_interested_bytes_in_hex.bin"));
 
         assert_eq!(not_interested_message.into_bytes(), expected_bytes);
     }
@@ -104,9 +89,7 @@ pub mod unittest {
     #[test]
     pub fn have_message_into_bytes() {
         let have_message = Have::new(0x1);
-
-        let expected_bytes =
-            read_bytes_from("samples/peer_wire_protocol-messages/expected_have_bytes_in_hex.bin");
+        let expected_bytes = read_bytes_from(&path_builder("expected_have_bytes_in_hex.bin"));
 
         assert_eq!(have_message.into_bytes(), expected_bytes);
     }

d1878ae17955b44b86d3aa13c5104ac8123d0b7f
Jean Fontena <diegofontena@gmail.com>
2022-11-13 16:35:47 +0100

    Improve variable naming

--- src/file_management/block_reader_writer.rs
+++ src/file_management/block_reader_writer.rs
@@ -9,14 +9,14 @@ use {
 
 pub struct BlockReaderWriter {
     piece_length: u32,
-    torrent_size: usize,
+    file_size: usize,
     file: File,
 }
 
 impl BlockReaderWriter {
     pub const BIT_TORRENT_BLOCK_SIZE: usize = 16 * 1024;
 
-    pub fn new(filepath: &Path, piece_length: u32, torrent_size: usize) -> Result<Self, Error> {
+    pub fn new(filepath: &Path, piece_length: u32, file_size: usize) -> Result<Self, Error> {
         let file = OpenOptions::new()
             .write(true)
             .read(true)
@@ -27,16 +27,16 @@ impl BlockReaderWriter {
         Ok(Self {
             piece_length,
             file,
-            torrent_size,
+            file_size,
         })
     }
 
-    pub fn write(&self, piece: u32, piece_offset: u32, data: &[u8]) -> Result<(), Error> {
+    pub fn write(&self, piece_index: u32, piece_offset: u32, data: &[u8]) -> Result<(), Error> {
         if data.len() > Self::BIT_TORRENT_BLOCK_SIZE as usize {
             return Err(Error::UnexpectedBlockSize);
         }
 
-        let offset = Self::calculate_offset(piece, self.piece_length(), piece_offset);
+        let offset = Self::calculate_offset(piece_index, self.piece_length(), piece_offset);
 
         self.file
             .write_at(data, offset.into())
@@ -45,8 +45,8 @@ impl BlockReaderWriter {
         Ok(())
     }
 
-    pub fn read(&self, piece: u32, piece_offset: u32) -> Result<Vec<u8>, Error> {
-        let offset = Self::calculate_offset(piece, self.piece_length(), piece_offset);
+    pub fn read(&self, piece_index: u32, piece_offset: u32) -> Result<Vec<u8>, Error> {
+        let offset = Self::calculate_offset(piece_index, self.piece_length(), piece_offset);
         let bytes_to_read = self.bytes_to_read(offset);
 
         let mut data = vec![0u8; bytes_to_read];
@@ -66,8 +66,8 @@ impl BlockReaderWriter {
     }
 
     fn bytes_to_read(&self, offset: u32) -> usize {
-        if (offset as usize + Self::BIT_TORRENT_BLOCK_SIZE) > self.torrent_size {
-            self.torrent_size - offset as usize
+        if (offset as usize + Self::BIT_TORRENT_BLOCK_SIZE) > self.file_size {
+            self.file_size - offset as usize
         } else {
             Self::BIT_TORRENT_BLOCK_SIZE
         }

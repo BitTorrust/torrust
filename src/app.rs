81f9352d926c249e52bab73271fabbc2484641ac
Jean Fontena <diegofontena@gmail.com>
2022-12-06 18:31:25 +0100

    Initialize logger based on --debug flag

--- src/app.rs
+++ src/app.rs
@@ -7,17 +7,22 @@ pub struct App {}
 
 impl App {
     pub fn run() -> Result<(), Error> {
+        let args = Args::parse();
+        if args.debug() {
+            Self::init_logger();
+        }
+
+        let torrent = Torrent::from_file(args.torrent_file())?;
+        let directory = args.working_directory();
+        StateMachine::new(torrent, directory).run();
+
+        Ok(())
+    }
+
+    fn init_logger() {
         SimpleLogger::new()
             .with_level(LevelFilter::Debug)
             .init()
             .unwrap();
-
-        let args = Args::parse();
-        let torrent = Torrent::from_file(args.torrent_file())?;
-        let directory = args.working_directory();
-
-        StateMachine::new(torrent, directory).run();
-
-        Ok(())
     }
 }

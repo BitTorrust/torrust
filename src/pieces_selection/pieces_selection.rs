ead86dcd627855bace61bf0b55cf62b657883806
Julien Liottard <julien.liottard@grenoble-inp.org>
2023-01-02 16:13:00 +0100

    simple pieces selction algorithm with unit tests

---
+++ src/pieces_selection/pieces_selection.rs
@@ -0,0 +1,9 @@
+use bit_vec::BitVec;
+use std::collections::HashMap;
+
+use crate::{http::Peer, Error};
+
+pub trait PiecesSelection {
+    /// Returns a associative table matching a piece_id with a peer for requesting the piece associated with the piece_id
+    fn pieces_selection(mybitfield: BitVec, peers_bitfields: HashMap<Peer, BitVec>) -> HashMap<u32, Option<Peer>>;
+}

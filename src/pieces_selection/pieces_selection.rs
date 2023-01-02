ef0123e074c06228407ae419c9093c18dc507362
Julien Liottard <julien.liottard@grenoble-inp.org>
2023-01-02 16:20:47 +0100

    cargo fmt

--- src/pieces_selection/pieces_selection.rs
+++ src/pieces_selection/pieces_selection.rs
@@ -5,5 +5,8 @@ use crate::{http::Peer, Error};
 
 pub trait PiecesSelection {
     /// Returns a associative table matching a piece_id with a peer for requesting the piece associated with the piece_id
-    fn pieces_selection(mybitfield: BitVec, peers_bitfields: HashMap<Peer, BitVec>) -> HashMap<u32, Option<Peer>>;
+    fn pieces_selection(
+        mybitfield: BitVec,
+        peers_bitfields: HashMap<Peer, BitVec>,
+    ) -> HashMap<u32, Option<Peer>>;
 }
--- src/pieces_selection/simple_selection.rs
+++ src/pieces_selection/simple_selection.rs
@@ -2,19 +2,22 @@ use std::collections::HashMap;
 
 use bit_vec::BitVec;
 
-use crate::{pieces_selection::PiecesSelection, http::Peer};
+use crate::{http::Peer, pieces_selection::PiecesSelection};
 
 #[derive(Debug)]
 pub struct SimpleSelector;
 
-impl PiecesSelection for SimpleSelector{
-    fn pieces_selection(mybitfield: BitVec, peers_bitfields: HashMap<Peer, BitVec>) -> HashMap<u32, Option<Peer>> {
+impl PiecesSelection for SimpleSelector {
+    fn pieces_selection(
+        mybitfield: BitVec,
+        peers_bitfields: HashMap<Peer, BitVec>,
+    ) -> HashMap<u32, Option<Peer>> {
         let mut piece_id_to_maybe_peer: HashMap<u32, Option<Peer>> = HashMap::new();
 
         for (current_peer, current_bitvec) in &peers_bitfields {
             for (piece_id, current_bit) in current_bitvec.iter().enumerate() {
-                let piece_id : u32 = piece_id as u32;
-                let current_piece_is_already_mine : bool = match mybitfield.get(piece_id as usize) {
+                let piece_id: u32 = piece_id as u32;
+                let current_piece_is_already_mine: bool = match mybitfield.get(piece_id as usize) {
                     Some(piece_is_present) => piece_is_present,
                     None => false,
                 };
@@ -26,13 +29,13 @@ impl PiecesSelection for SimpleSelector{
                         } else {
                             piece_id_to_maybe_peer.insert(piece_id, None);
                         }
-                    },
+                    }
                     Some(None) => {
                         // Update the peer value if it is a None
                         if !current_piece_is_already_mine {
                             piece_id_to_maybe_peer.insert(piece_id, Some(current_peer.clone()));
                         }
-                    },
+                    }
                     Some(Some(_)) => (),
                 }
             }

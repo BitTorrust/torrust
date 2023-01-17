66bd2a83dfe72553a9442678f3ef2523afeeaf13
Juan Jose Garcia Duarte <garciadj@grenoble-inp.org>
2023-01-17 17:16:23 +0100

    Replaced gnome commands on multi-seeders-iceberg scripts to be compatible with no gnome users

--- scripts/multi-seeders-iceberg.sh
+++ scripts/multi-seeders-iceberg.sh
@@ -4,10 +4,10 @@ TORRENT_FILE=$1         # given torrent file  as the first argument
 WORKING_DIR=$2          # given directory where the is going to be saved
 WORKING_DIR_ARIAS=$3    # given directory where the file to upload is stored
 
-gnome-terminal -- aria2c -V -d $WORKING_DIR_ARIAS/aria1-4/ $TORRENT_FILE --listen-port=2002 
-gnome-terminal -- aria2c -V -d $WORKING_DIR_ARIAS/aria5-8/ $TORRENT_FILE --listen-port=2001 
-gnome-terminal -- aria2c -V -d $WORKING_DIR_ARIAS/aria9-11/ $TORRENT_FILE --listen-port=2003 
+aria2c -V -d $WORKING_DIR_ARIAS/aria1-4/ $TORRENT_FILE --listen-port=2002 &
+aria2c -V -d $WORKING_DIR_ARIAS/aria5-8/ $TORRENT_FILE --listen-port=2001 &
+aria2c -V -d $WORKING_DIR_ARIAS/aria9-11/ $TORRENT_FILE --listen-port=2003 &
 
+sleep 5 &&
 
-
-gnome-terminal -- cargo run -- $TORRENT_FILE $WORKING_DIR --debug --mock
+cargo run --release -- $TORRENT_FILE $WORKING_DIR --debug --mock

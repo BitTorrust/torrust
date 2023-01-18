b0491e56a856e98e10166c1b6c9198e139f5151c
Juan Jose Garcia Duarte <garciadj@grenoble-inp.org>
2023-01-18 17:28:33 +0100

    Fix tracker.sh script, it had unexpected - symbol at the beginning of each line

--- scripts/tracker.sh
+++ scripts/tracker.sh
@@ -1,8 +1,8 @@
--#!/bin/bash
--
--OPENTRACKER_IP_ADDRESS="127.0.0.1"
--OPENTRACKER_PORT=6969
--
--opentracker -i $OPENTRACKER_IP_ADDRESS -p $OPENTRACKER_PORT
--
--pkill opentracker
+#!/bin/bash
+
+OPENTRACKER_IP_ADDRESS="127.0.0.1"
+OPENTRACKER_PORT=6969
+
+opentracker i $OPENTRACKER_IP_ADDRESS p $OPENTRACKER_PORT
+
+pkill opentracker
